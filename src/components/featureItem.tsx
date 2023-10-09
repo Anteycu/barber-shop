bd404e02280f1192b5ffc21b4ec34003976c2f5c
Nikolay Trunov <anteycu@gmail.com>
2023-10-09 15:08:57 +0300

    edit some font sizes

--- src/components/featureItem.tsx
+++ src/components/featureItem.tsx
@@ -8,11 +8,11 @@ interface Props {
 export const FeatureItem = ({ children, title, src, order }: Props) => {
   return (
     <div
-      className={`flex ${order} justify-around items-center mb-20 text-lg font-medium text-yellow-900`}
+      className={`flex ${order} justify-around items-center mb-20 text-xl font-medium text-yellow-900`}
     >
-      <img className="w-1/3 rounded-3xl" src={src} alt="Barber works" />
+      <img className="w-1/3 rounded-4xl" src={src} alt="Barber works" />
       <div className="w-1/3">
-        <h2>{title}</h2>
+        <h2 className="text-3xl">{title}</h2>
         <p>{children}</p>
       </div>
     </div>
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
   return (
     <a>
-      <p className="text-2xl font-bold text-yellow-900">
+      <p className="text-3xl font-bold text-yellow-900">
         Chocolate<span className="text-yellow-600">Woods</span>
       </p>
     </a>
